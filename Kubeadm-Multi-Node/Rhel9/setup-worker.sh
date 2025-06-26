db510ed5c18f7b282bad53588a1f455a61147dfa
Sayantan <sayantansamanta12102001@gmail.com>
2025-06-26 19:09:05 +0530

    updated  setup script

--- Kubeadm-Multi-Node/Rhel9/setup-worker.sh
+++ Kubeadm-Multi-Node/Rhel9/setup-worker.sh
@@ -72,5 +72,5 @@ echo "✅ Kubernetes common setup completed successfully."
 kubeadm join <master-ip>:6443 --token <token> --discovery-token-ca-cert-hash <hash>
 
 # Example
-kubeadm join 172.31.84.118:6443 --token 7b477r.fbnsaxafe77nzlik \
-        --discovery-token-ca-cert-hash sha256:87a91c9c8bcf70510576bb76f13d972a7edb7249980a800ed4c3d9b658af05b7
+# kubeadm join 172.31.84.118:6443 --token 7b477r.fbnsaxafe77nzlik \
+#         --discovery-token-ca-cert-hash sha256:87a91c9c8bcf70510576bb76f13d972a7edb7249980a800ed4c3d9b658af05b7
